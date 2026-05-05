4d9492bd21dec86df6b4e1b6787594507d489e70
MacmillanMatlaila <macmillanmatlaila@gmail.com>
2026-05-06 00:18:25 +0200

    Student Registration app became student database app
    In this app I learned about coverting form entries into an object, I can redo my NotesApp using this knowledge.
    
    I also learned how to use newRow, insertCell and insertRow
    
    I learned it's not always necessary to use a loop but my obsession with loops when it comes to arrays and objects is actually justified to be fair but I need to look out for

--- Student-Register-App/javascript/script.js
+++ Student-Register-App/javascript/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-// Form variable
+// Form variables
 let form = document.querySelector("main section form")
 
 // Personal Info Variables
@@ -17,48 +17,92 @@ let email = document.getElementById("email")
 // Course Details Variables
 let faculty = document.getElementById("faculty")
 let course = document.getElementById("course")
-let diploma = document.getElementById("diploma")
-let degree = document.getElementById("degree")
-let masters = document.getElementById("masters")
-let phd = document.getElementById("phd")
-
-// Button Variables
-let registerBtn = document.getElementById("registerBtn")
-
-// localStorage variable
-let studentArray = JSON.parse(localStorage.getItem("studentArray")) || []
+let level = document.getElementById("level")
+let yearOfStudy = document.getElementById("year_of_study")
 
 
-function createStudent(){
-    let student = {}
-    let formLabel = document.querySelectorAll("main section form fieldset p label")
-    let formInput = document.querySelectorAll("main section form fieldset p input")
+// Table Variables
+let tbody = document.getElementById("tbody")
 
-    for(let i=0; i<formInput.length; i++){ 
-        let key = formLabel[i].innerText.replace(/ /g, "_")
-        let value = formInput[i].value
-        // var student = `{${formLabel[i].innerHTML.replace(" ", "_").replace(" ", "_")}: '${formInput[i].value}'}`
-        student[key] = key
-        student[value] = value
-        console.log(student)
-    }
 
-    return student
+let studentsArray = JSON.parse(localStorage.getItem("studentsArray")) || []
+
+if(studentsArray){
+    studentsArray.forEach(student => {
+        let row = `<tr>
+                            <td>${student.name}</td>
+                            <td>${student.surname}</td>             
+                            <td>${student.id}</td>  
+                            <td>${student.age}</td>  
+                            <td>${student.gender}</td>  
+                            <td>${student.race}</td>  
+                            <td>${student.student_number}</td>  
+                            <td>${student.number}</td>  
+                            <td>${student.email}</td>  
+                            <td>${student.faculty}</td>  
+                            <td>${student.course}</td>  
+                            <td>${student.level}</td>  
+                            <td>${student.year_of_study}</td>  
+                       </tr>`
+
+        tbody.innerHTML += row
+    })
 }
 
 
-registerBtn.addEventListener('click', function(event){
-    event.preventDefault()
 
-    let newStudent = createStudent()
+form.addEventListener('submit', function(e){
+    e.preventDefault()
 
-    if(newStudent){
-        studentArray.push(newStudent)
+    // 1. Create a formData object from the form
+    const formData = new FormData(e.target)
 
-        localStorage.setItem("studentArray", JSON.stringify(studentArray))
+    // 2. Convert entries to a single object
+    const studentObject = Object.fromEntries(formData.entries())
 
-        console.log(studentArray)
+    // 3. Push object into array
+
+    if(studentObject){
+        studentsArray.push(studentObject)
+
+        localStorage.setItem("studentsArray", JSON.stringify(studentsArray))
     }
 
-    console.log("student added")
+    // Displaying the object data using a method better than innerHTML to avoid injection
+
+    const newRow = tbody.insertRow(-1) // "-1" means last, so each row is added last
+
+    const nameCell = newRow.insertCell(0)
+    const surnameCell = newRow.insertCell(1)
+    const idCell = newRow.insertCell(2)
+    const ageCell = newRow.insertCell(3)
+    const genderCell = newRow.insertCell(4)
+    const raceCell = newRow.insertCell(5)
+    const studentNumberCell = newRow.insertCell(6)
+    const contactNumberCell = newRow.insertCell(7)
+    const emailCell = newRow.insertCell(8)
+    const facultyCell = newRow.insertCell(9)
+    const courseCell = newRow.insertCell(10)
+    const courseLevelCell = newRow.insertCell(11)
+    const yearOfStudyCell = newRow.insertCell(12)
+
+    nameCell.textContent = studentObject.name
+    surnameCell.textContent = studentObject.surname
+    idCell.textContent = studentObject.id
+    ageCell.textContent = studentObject.age
+    genderCell.textContent = studentObject.gender
+    raceCell.textContent = studentObject.race
+    studentNumberCell.textContent = studentObject.student_number
+    contactNumberCell.textContent = studentObject.number
+    emailCell.textContent = studentObject.email
+    faculty.textContent = studentObject.faculty
+    courseCell.textContent = studentObject.course
+    courseLevelCell.textContent = studentObject.level
+    yearOfStudyCell.textContent = studentObject.year_of_study
+
+
+
+    console.log(studentsArray)
+    e.target.reset()
+
 })
