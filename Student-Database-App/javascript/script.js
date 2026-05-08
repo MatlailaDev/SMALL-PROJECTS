47617947b8506d49b4e6b79e17f07ceeb5839805
MacmillanMatlaila <macmillanmatlaila@gmail.com>
2026-05-08 21:52:19 +0200

    Added Search functionality using filter() and using an eventListener on the parent element of cards created using search results and their data.id I can now access the originals complete objects from search results and use them to display specific and more comprehensive information about the clicked card. What an exciting and amazing thing to learn i am already excited about using this to build my store.

--- Student-Database-App/javascript/script.js
+++ Student-Database-App/javascript/script.js
@@ -1,7 +1,16 @@
 "use strict"
 
-// Navigation variables
+// Search variables
+let mainElement = document.getElementById("main") 
+let searchSection = document.querySelector(".searchSection")
 let searchForm = document.getElementById("searchForm")
+let searchBtn = document.getElementById("searchBtn")
+let searchFormInput = document.getElementById("search")
+let studentCardsSection = document.getElementById("studentCardsSection")
+let numberOfStudentsFound = document.querySelector("main #studentCardsSection h2")
+let cardsContainer = document.querySelector("main #studentCardsSection span")
+let close = document.getElementById("close")
+let specificStudentInformationSection = document.getElementById("specificStudentInformationSection")
 
 
 // Form variables
@@ -29,7 +38,7 @@ let yearOfStudy = document.getElementById("year_of_study")
 let tableDiv = document.getElementById("tableDiv")
 let tbody = document.getElementById("tbody")
 
-
+// Runs on page load
 let studentsArray = JSON.parse(localStorage.getItem("studentsArray")) || []
 
 if(studentsArray){
@@ -54,8 +63,166 @@ if(studentsArray){
             tbody.innerHTML += row
         }
     })
+
+
+    if(searchForm){
+            searchBtn.addEventListener('click', function(e){
+                e.preventDefault()
+
+                const searchInput = searchFormInput.value 
+
+                const searchResults = studentsArray.filter(student => {
+                    // over both the data and search termto lowercase for better match
+                    const nameMatch = student.name.toLowerCase().includes(searchInput.toLowerCase())
+                    const surnameMatch = student.surname.toLowerCase().includes(searchInput.toLowerCase())
+                    const concatMatch = `${student.name} ${student.surname}`.toLowerCase().includes(searchInput.toLowerCase())
+                    const studentNumberMatch = student.student_number.includes(searchInput)
+                    const courseMatch =student.course.toLowerCase().includes(searchInput.toLowerCase())
+
+                    return nameMatch || surnameMatch || concatMatch || studentNumberMatch || courseMatch 
+                })
+
+                console.log(`Search input is ${searchInput}`)
+                console.log(`${searchResults.length} results from array match your search`)
+
+
+
+                
+                
+                if(searchInput !== ""){
+
+                    studentCardsSection.style.display = 'block'
+
+                    numberOfStudentsFound.textContent = `Students Found: ${searchResults.length}`
+
+                    cardsContainer.replaceChildren()
+
+                    searchResults.forEach(result => {
+                        let studentCard = `<div class="studentCard" data-id="${result.id}">
+                                                <div class = "cardContents">
+                                                    <div class = "logo">
+                                                        <i class="fa fa-university" aria-hidden="true"></i>
+                                                        <h4>Javascript University</h4>
+                                                    </div>
+
+                                                    <div class="details">
+                                                        <ul>
+                                                            <li>${result.name} ${result.surname}</li>
+                                                            <li>${result.student_number}</li>
+                                                            <li>${result.course}</li>
+                                                        </ul>
+                                                        <img class="profile-image" src="/Student-Database-App/assets/profile-images/1.jpg" alt="myimage">
+                                                    </div>
+
+                                                
+                                                    <h5>Student Card 2018</h5>
+                                                </div>    
+                                            </div>`
+                        
+                            cardsContainer.innerHTML += studentCard
+
+                    })
+                        console.log(`${document.querySelectorAll(".studentCard").length} student cards were created, does this match the number of search results?`)
+                        
+
+                }else{
+                    studentCardsSection.style.display = 'none'
+                    console.log(`search Input is ${searchInput.length} as such no results were shown`)
+                } //This is the end of the if else statement
+
+                
+                
+                if(searchResults.length > 0 && searchInput.length !== 0){
+                    searchSection.style.display = 'none'
+                    close.style.display = 'flex'
+                    document.body.style.backgroundColor = '#EEE'
+                }
+
+            })
+    }
 }
 
+cardsContainer.addEventListener('click', function(e){
+                            if(e.target.classList.contains("profile-image")){
+                                // Get the id from the parent card
+                                const targetId = e.target.closest(".studentCard").dataset.id
+
+                                // Access the specific object
+                                const specificStudentObject = studentsArray.find(obj => obj.id == targetId)
+
+                                displaySpecificStudentInformation(specificStudentObject)
+
+
+
+                            }
+})
+
+function displaySpecificStudentInformation(specificStudentObject){
+    close.style.display = 'flex'
+    specificStudentInformationSection.style.display = 'block'
+    document.body.style.backgroundSize = '0%'
+    document.body.style.backgroundColor = '#EEE'
+    searchSection.style.display = 'none'
+    studentCardsSection.style.display = 'none'
+
+    let specificStudentInformation = `
+                                      <h2>Student Information for <span>${specificStudentObject.name} ${specificStudentObject.surname}: ${specificStudentObject.student_number}</span></h2>
+                                      <div class="specificStudentDetails">
+                                        <table>
+                                            <caption>Student Information</caption>
+
+                                            <thead>
+                                                <th>Student Picture</th>
+                                                <th>Name</th>
+                                                <th>Surname</th>
+                                                <th>ID</th>
+                                                <th>Age</th>
+                                                <th>Gender</th>
+                                                <th>Race</th>
+                                                <th>Student Number</th>
+                                                <th>Contact Number</th>
+                                                <th>Email</th>
+                                                <th>Faculty</th>
+                                                <th>Course</th>
+                                                <th>Course Level</th>
+                                                <th>Year Of Study</th>
+                                            </thead>
+
+                                            <tbody>
+                                                <tr>
+                                                    <td class="student-picture">${specificStudentObject.image}</td>
+                                                    <td>${specificStudentObject.name}</td>
+                                                    <td>${specificStudentObject.surname}</td>
+                                                    <td>${specificStudentObject.id}</td>
+                                                    <td>${specificStudentObject.age}</td>
+                                                    <td>${specificStudentObject.gender}</td>
+                                                    <td>${specificStudentObject.race}</td>
+                                                    <td>${specificStudentObject.student_number}</td>
+                                                    <td>${specificStudentObject.contact_number}</td>
+                                                    <td>${specificStudentObject.email}</td>
+                                                    <td>${specificStudentObject.faculty}</td>
+                                                    <td>${specificStudentObject.course}</td>
+                                                    <td>${specificStudentObject.level}</td>
+                                                    <td>${specificStudentObject.year_of_study}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                      </div>`
+                                      
+    specificStudentInformationSection.innerHTML = specificStudentInformation
+}
+
+if(close){
+    close.addEventListener('click', function(){
+        specificStudentInformationSection.style.display = 'none'
+        searchSection.style.display = 'block'
+        studentCardsSection.style.display = 'none'
+        document.body.style.backgroundSize = '100%'
+        document.body.style.backgroundColor = '#000000'
+        close.style.display = 'none'
+
+    })
+}
 
 
 if(form && !searchForm){
