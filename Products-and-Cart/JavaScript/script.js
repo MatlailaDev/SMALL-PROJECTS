3354279a47be2183370ea71e6972cf2699cefe2d
MacmillanMatlaila <macmillanmatlaila@gmail.com>
2026-05-15 11:11:26 +0200

    I am exploring the adding an evetListener to the parent container by building a small shop, it's basically just an array full objects that are displayed on screen using a for each, then I added an add-to-cart button that targets the id of the clicked element, then uses that to target the original object ued to create it and adds that object to an array. I also added an increment and decrement feature where I learned that you can target a specific button click and display div on a card by adding an event listener to the parent container of the cards and using e.target.classList.contains("") , e.target points to the paren container and the .contains("") then essentially targets the element to be clicked within the e.target(parennt container). You can also pair this with querySelector and probably other DOM methods

--- Products-and-Cart/JavaScript/script.js
+++ Products-and-Cart/JavaScript/script.js
@@ -1,91 +1,115 @@
 let productsContainer = document.getElementById("productsContainer")
+let sideCartItemsContainer = document.getElementById("sideCartItemsContainer")
+let numberOfItemsInCart = document.querySelector(".cart-heading")
+let productPage = document.getElementById("productPage")
 
 
 
 let productsArray = [
     {
+        id: crypto.randomUUID(),
         productName: "Broccoli",
         productPrice: 30,
-        productDescription:"",
+        productDescription:"Lorem.",
         productImage: "./assets/products/broccoli.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Peppers",
         productPrice: 10,
-        productDescription: "",
+        productDescription: "Lorem ipsum.",
         productImage: "./assets/products/green-red-yellow-pepper.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Eggplant",
         productPrice: 68,
-        productDescription: "",
+        productDescription: "Lorem, ipsum dolor.",
         productImage: "./assets/products/eggplant.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Cucumber",
         productPrice: 40,
-        productDescription: "",
+        productDescription: "Lorem ipsum dolor sit.",
         productImage: "./assets/products/cucumber.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Lettuce",
         productPrice: 29,
         productDescription: "",
         productImage: "./assets/products/lettuce.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Red Pepper",
         productPrice: 18,
         productDescription: "",
         productImage: "./assets/products/red-pepper.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Garlic",
         productPrice: 36,
         productDescription: "",
         productImage: "./assets/products/garlic.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Onions",
         productPrice: 19,
         productDescription: "",
         productImage:"./assets/products/Onions.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Beetroot",
         productPrice: 29,
         productDescription: "",
         productImage:"./assets/products/beetroot.jpg",
+        quantity : 1
     },
     {
+        id: crypto.randomUUID(),
         productName: "Carrots",
         productPrice: 35,
         productDescription: "",
         productImage:"./assets/products/carrots.jpg",
+        quantity : 1
     }
 ]
 
-// let cartArray = JSON.parse(localStorage.getItem(cartArray)) || []
+const cartArray = JSON.parse(localStorage.getItem("cartArray")) || []
 
-function openSideCart(){
-    document.getElementById("sideCart").style.width = "350px"
-    document.querySelector("main").style.marginRight = "350px"
-}
-
-function closeSideCart(){
-    document.getElementById("sideCart").style.width = "0"
-    document.querySelector("main").style.marginRight = "0"
-}
 
 // create productCard element
 function createProduct(productObjectParameter){
     return `<div class = "productCard" data-id = "${productObjectParameter.id}">
+                        <div class = "cardContents">    
                             <img src = "${productObjectParameter.productImage}">
                             <div class = "productDetails">
                                 <h4>${productObjectParameter.productName}</h4>
-                                <p>${productObjectParameter.productPrice}</p>
+                                <p>R${productObjectParameter.productPrice}</p>
                             </div>
-                       </div>`
+                            <div class = "wrapper">
+                                <a id = "add-to-cart" class = "add-to-cart" onclick = "openSideCart()">Add to cart</a>
+                                <div class = "quantity_display">${productObjectParameter.quantity}</div>
+                                <span>
+                                    <button class = "increment">+</button>
+                                    <button class = "decrement">-</buttton>
+                                </span>
+                            </div>
+                        </div>
+            </div>`
 
     
 }
@@ -96,4 +120,158 @@ productsArray.forEach(product => {
     let productCard = createProduct(product)
     productsContainer.innerHTML += productCard
 })
+
+//Onclick functions for cart
+function openSideCart(){
+    document.getElementById("sideCart").style.width = "450px"
+    document.querySelector("main").style.marginRight = "450px"
+}
+
+function closeSideCart(){
+    document.getElementById("sideCart").style.width = "0"
+    document.querySelector("main").style.marginRight = "0"
+}
+
+// Onclick functions to increment and decrement product quantity
+if(productsContainer){
+
+    productsContainer.addEventListener('click', function(e){
+        if(e.target.classList.contains("increment")){
+            const display = e.target.closest(".productCard").querySelector(".quantity_display")
+            display.textContent = parseInt(display.textContent) + 1
+        }
+
+        if(e.target.classList.contains("decrement")){
+            const display = e.target.closest(".productCard").querySelector(".quantity_display")
+            const current = parseInt(display.textContent)
+           if(current > 1){
+                display.textContent = current - 1
+           }
+        }  
+        
+    })
+}
+
+
+
+
+// I want to click addToCart and add the clicked product to cart by accessing its original object and pushing it to cartArray then using it to create a cart item
+// So the first thing is to access the original object
+
+if(productsContainer){
+    productsContainer.addEventListener('click', function(e){
+        if(e.target.classList.contains("add-to-cart")){
+            // Get the id from the parent card element
+            const targetId = e.target.closest(".productCard").dataset.id
+
+            // target the Parent element itself, I want to target the input value for increment/decrement
+            const targetElement = e.target.closest("[data-id]")
+
+            // Now that I have the Id, I can access the specific productObject
+            const specificProductObject = productsArray.find(obj => obj.id == targetId)
+
+            // Add Quantity key/value to object
+            specificProductObject.quantity = e.target.closest(".productCard").querySelector(".quantity_display").textContent
+            // newSpecificProductObject = {
+            //     ...specificProductObject,
+            //     newQuantity: e.target.closest(".productCard").document.querySelector(".quantity_display").value
+            // }
+            
+            // Call the function that pushes this object to the cart array
+            pushToCart(specificProductObject)
+        
+
+            
+        }
+    })
+}
+
+// Helper for pushing items to cartArray and storing them in localStorage when add-to-cart is clicked
+function pushToCart(specificProductObject){
+    // let cartArray = JSON.parse(localStorage.getItem("cartArray")) || []
+
+
+    cartArray.push(specificProductObject)
+
+    localStorage.setItem("cartArray", JSON.stringify(cartArray))
+
+
+    console.log(cartArray)
+}
+
+// Time to create cart items 
+function createCartItems(specificProductObject){
+    return `<div class = "cartProductCard" data-id = "${specificProductObject.id}">
+                <div class = "cartProductCardContents">
+                    <img src = "${specificProductObject.productImage}">
+                    <div class = "cartProductDetails">
+                        <h4>${specificProductObject.productName}</h4>
+                        <p>R${parseFloat(specificProductObject.productPrice*specificProductObject.quantity)}</p>
+                        <div class = "wrapper">
+                            <div class = "x">X</div>
+                            <div class = "quantity_display">${specificProductObject.quantity}</div>
+                            <span>
+                                <button class = "increment">+</button>
+                                <button class = "decrement">-</buttton>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+cartArray.forEach(cartProduct => {
+    let cartProductItem = createCartItems(cartProduct)
+    sideCartItemsContainer.innerHTML += cartProductItem
+    // Although fror now it makes sense to count the number of items in cart array the true count will be the count of items including their quantity
+    numberOfItemsInCart.innerHTML = `<h4>There are <span>${cartArray.length} items</span> in your cart</h4>`
+})
+
+
+if(sideCartItemsContainer){
+    sideCartItemsContainer.addEventListener('click', function(e, specificProductObject){
+        if(e.target.classList.contains("increment")){
+            const display = e.target.closest(".cartProductCard").querySelector(".quantity_display")
+            display.textContent = Number(display.textContent) + 1
+            
+        }
+
+        if(e.target.classList.contains("decrement")){
+            const display = e.target.closest(".cartProductCard").querySelector(".quantity_display")
+            const current = parseInt(display.textContent)
+            if(current > 1){
+                display.textContent = current - 1
+            }
+        }
+
+        
+
+
+    })
+}
+
+
+
+
+
+// Product Page Functionality
+if(productPage || productsContainer){
+
+    productsContainer.addEventListener('click', function(e){
+        if(e.target.classList.contains(".productCard")){
+            // Get the id from the parent container
+            const targetId = e.target.closest(".productCard").dataset.id
+
+            const specificProductObject = productsArray.find(obj => obj.id == targetId)
+
+            displaySpecificProduct(specificProductObject)
+        }
+    })
+}
+
+function displaySpecificProduct(specificProductObject){
+    let specificProductInformation = `<div>${specificProductObject.description}</div>`
+
+    productPage.innerHTML = specificProductInformation
+}
 
